f7a6d29f7014316854f992daefd17a05b5ccd0f5
Nils <nilsdu29@gmail.com>
2016-09-10 17:25:30 +0200

    Création du controlleur Chat

---
+++ src/CoreBundle/Controller/ChatController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\PreconditionRequiredHttpException;
+
+use CoreBundle\Entity\Chat;
+
+class ChatController extends Controller
+{
+	const NB_MESSAGES_A_AFFICHER = 15;
+
+	public function viewAction()
+	{
+		return $this->render('CoreBundle:Chat:view.html.twig');
+	}
+
+	public function sendAction(Request $request)
+	{
+		$message = $request->request->get('message');
+		if (!$message) throw new PreconditionRequiredHttpException('No message was sent.');
+		
+		$user = $this->getUser();
+
+		$chat = new Chat();
+
+		$chat
+			->setUsername($user->getUsername())
+			->setMessage($message);
+
+		$em = $this->getDoctrine()->getManager();
+
+		$em->persist($chat);
+		$em->flush();
+
+		return new Response('', 201);
+	}
+
+	public function getmessagesAction()
+	{
+		$repository = $this->getDoctrine()
+			->getManager()
+			->getRepository('CoreBundle:Chat');
+
+		$messages = $repository->getLast(self::NB_MESSAGES_A_AFFICHER);
+
+		foreach ($messages as $key => $message) {
+			$dateInterval = $message['sentTime']->diff(new \DateTime);
+			if ($dateInterval->days != 0) {
+				$string = $dateInterval->format('%aj %hh %im %ss');
+			} elseif ($dateInterval->h != 0) {
+				$string = $dateInterval->format('%hh %im %ss');
+			} elseif ($dateInterval->i != 0) {
+				$string = $dateInterval->format('%im %ss');
+			} else {
+				$string = $dateInterval->format('%ss');
+			}
+			$messages[$key]['ilYA'] = $string;
+		}
+
+		return $this->render(
+			'CoreBundle:Chat:getmessages.html.twig',
+			array(
+				'messages' => $messages,
+				)
+			);
+	}
+}
---
+++ src/CoreBundle/Entity/Chat.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Chat
+ *
+ * @ORM\Table(name="chat")
+ * @ORM\Entity(repositoryClass="CoreBundle\Repository\ChatRepository")
+ */
+class Chat
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="text")
+     */
+    private $message;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="sent_time", type="datetime")
+     */
+    private $sentTime;
+
+    function __construct()
+    {
+        $this->sentTime = new \DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return Chat
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Chat
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set sentTime
+     *
+     * @param \DateTime $sentTime
+     *
+     * @return Chat
+     */
+    public function setSentTime($sentTime)
+    {
+        $this->sentTime = $sentTime;
+
+        return $this;
+    }
+
+    /**
+     * Get sentTime
+     *
+     * @return \DateTime
+     */
+    public function getSentTime()
+    {
+        return $this->sentTime;
+    }
+}
+
