772143c63fa82afeba6470127737fe16c4382efa
Nils <nilsdu29@gmail.com>
2016-09-10 17:53:33 +0200

    Mise en fonction de la page de contact. Modification de l'emplacement des messages flash.

--- src/CoreBundle/Controller/IndexController.php
+++ src/CoreBundle/Controller/IndexController.php
@@ -10,48 +10,53 @@ use CoreBundle\Form\ContactType;
 
 class IndexController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('CoreBundle:Index:index.html.twig', array(
-            // ...
-        ));
-    }
+	public function indexAction()
+	{
+		return $this->render('CoreBundle:Index:index.html.twig');
+	}
 
-    public function contactAction(Request $request)
-    {
-        $mail = new ContactModel();
-        $form = $this->get('form.factory')->create(ContactType::class, $mail);
+	public function contactAction(Request $request)
+	{
+		$mail = new ContactModel();
+		$form = $this->get('form.factory')->create(ContactType::class, $mail);
 
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-            # envoi de l'e-mail
-            
-            return $this->redirectToRoute('index');
-        }
+		if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+			
+			$email = new \Swift_Message;
+			$email->setSubject('Contact – '.$mail->getObjet());
+			$email->setReplyTo($mail->getEmail(), $mail->getNom().' '.$mail->getPrenom());
+			$email->setFrom($mail->getEmail());
+			$email->setBody('Envoyé par '.$mail->getNom().' '.$mail->getPrenom().'( '.$mail->getEmail().' )
 
-        return $this->render('CoreBundle:Index:contact.html.twig', array(
-            'form' => $form->createView()
-        ));
-    }
+'.$mail->getContenu(), 'text/plain');
+			$email->setTo($this->container->getParameter('mailer_user'));
+			$email->setSender($this->container->getParameter('mailer_user'));
 
-    public function linksAction()
-    {
-        return $this->render('CoreBundle:Index:links.html.twig', array(
-            // ...
-        ));
-    }
+			$this->get('mailer')->send($email);
 
-    public function termsAction()
-    {
-        return $this->render('CoreBundle:Index:terms.html.twig', array(
-            // ...
-        ));
-    }
+			$request->getSession()->getFlashBag()->add('success', 'Votre e-mail a bien été envoyé')
+			
+			return $this->redirectToRoute('index');
+		}
 
-    public function creditsAction()
-    {
-        return $this->render('CoreBundle:Index:credits.html.twig', array(
-            // ...
-        ));
-    }
+		return $this->render('CoreBundle:Index:contact.html.twig', array(
+			'form' => $form->createView()
+		));
+	}
+
+	public function linksAction()
+	{
+		return $this->render('CoreBundle:Index:links.html.twig');
+	}
+
+	public function termsAction()
+	{
+		return $this->render('CoreBundle:Index:terms.html.twig');
+	}
+
+	public function creditsAction()
+	{
+		return $this->render('CoreBundle:Index:credits.html.twig');
+	}
 
 }
--- src/CoreBundle/FormModels/ContactModel.php
+++ src/CoreBundle/FormModels/ContactModel.php
@@ -2,13 +2,35 @@
 
 namespace CoreBundle\FormModels;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class ContactModel
 {
+	/**
+	 * @Assert\Length(min=3, minMessage="Doit faire au moins {{ limit }} caractères")
+	 */
 	private $nom;
+
+	/**
+	 * @Assert\Length(min=3, minMessage="Doit faire au moins {{ limit }} caractères")
+	 */
 	private $prenom;
+
+	/**
+	 * @Assert\Email(checkMX=true, message="Veuillez entrer une adresse e-mail existante")
+	 */
 	private $email;
+
+	/**
+	 * @Assert\Length(max=255, min=3, maxMessage="Doit faire au plus {{ limit }} caractères", minMessage="Doit faire au moins {{ limit }} caractères")
+	 */
 	private $objet;
+
+	/**
+	 * @Assert\Length(min=10, minMessage="Doit faire au moins {{ limit }} caractères")
+	 */
 	private $contenu;
+
 
 	public function getNom()
 	{
