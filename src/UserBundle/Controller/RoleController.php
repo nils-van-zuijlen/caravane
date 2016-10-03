e5b39fb820ba935d2efa6873e0b95e0c02849066
Nils <nilsdu29@gmail.com>
2016-10-03 21:19:15 +0200

    Création du controlleur de modification des rôles

---
+++ src/UserBundle/Controller/RoleController.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+
+/**
+ * RESTful controller managing group CRUD
+ *
+ * @author Thibault Duplessis <thibault.duplessis@gmail.com>
+ * @author Christophe Coevoet <stof@notk.org>
+ */
+class RoleController extends Controller
+{
+	public function viewAction(Request $request)
+	{
+		$users = $this
+			->getDoctrine()
+			->getManager()
+			->getRepository('UserBundle:User')
+			->findAll();
+
+		return $this->render(
+			'UserBundle:Role:view.html.twig',
+			array(
+				'users' => $users,
+				)
+			);
+	}
+
+	/**
+	 * @Security("has_role('ROLE_ALLOWED_TO_SWITCH')")
+	 */
+	public function editAction(Request $request, $username)
+	{
+		$em = $this
+			->getDoctrine()
+			->getManager();
+
+		$user = $em
+			->getRepository('UserBundle:User')
+			->findOneByUsername($username);
+
+		if ($user === null) {
+			throw NotFoundHttpException(
+				'L\'utilisateur '.$username.' n\'existe pas.'
+				);
+		}
+
+		$form = $this->createForm(RoleType::class, $user);
+
+		if (
+			$request->isMethod('POST')
+			&& $form->handleRequest($request)->isValid()
+			) {
+
+			$em->persist($user);
+			$em->flush();
+
+			$request
+				->getSession()
+				->getFlashBag()
+				->add('success', 'Les rôles de l\'utilisateur '.$username.' ont bien été modifiés');
+
+			return $this->redirect('user_role_view');
+		}
+
+		return $this->render(
+			'UserBundle:Role:edit.html.twig',
+			array(
+				'form' => $form->createView(),
+				)
+			);
+	}
+}
