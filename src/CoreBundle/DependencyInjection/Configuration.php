53672d4095592fccbf4f44bd79f3b56bfea6a2e2
Nils <nilsdu29@gmail.com>
2016-10-06 17:42:38 +0200

    Composer Update

---
+++ src/CoreBundle/DependencyInjection/Configuration.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CoreBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getConfigTreeBuilder()
+	{
+		$treeBuilder = new TreeBuilder();
+		$rootNode = $treeBuilder->root('core');
+		return $treeBuilder;
+	}
+}
