2a99c0f7bdbf6a9e93caf14f524a8d17801ca419
Nils Van Zuijlen <nilsdu29@gmail.com>
2016-10-12 11:20:56 +0200

    Update IntendanceController.php
    Edited ResponsabilitesBundle:Intendance:viewMenu
    It now search the menu in database and throws an exception if not found.

--- src/ResponsabilitesBundle/Controller/IntendanceController.php
+++ src/ResponsabilitesBundle/Controller/IntendanceController.php
@@ -21,10 +21,19 @@ class IntendanceController extends Controller
 
 	public function viewMenuAction(Request $request, $slug)
 	{
+		$menu = $this
+			->getDoctrine()
+			->getManager()
+			->getRepository('ResponsabilitesBundle:Menu')
+			->findOneBySlug((string) $slug);
+		
+		if ($menu === null)
+			throw $this->createNotFoundException('Le menu '.$slug.' n\'existe pas');
+		
 		return $this->render(
 			'ResponsabilitesBundle:Intendance:view_menu.html.twig',
 			array(
-				#
+				'menu' => $menu,
 				)
 			);
 	}
