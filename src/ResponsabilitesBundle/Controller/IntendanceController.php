d5ad4a2f3b2e09e552849d82a51f3158b27d4e47
Nils Van Zuijlen <nilsdu29@gmail.com>
2016-10-12 11:51:38 +0200

    Update IntendanceController.php
    Added editing and creating menus.

--- src/ResponsabilitesBundle/Controller/IntendanceController.php
+++ src/ResponsabilitesBundle/Controller/IntendanceController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
+use ResponsabilitesBundle\Entity\Menu;
+use ResponsabilitesBundle\Form\Type\MenuFormType;
+
 class IntendanceController extends Controller
 {
 	public function indexAction(Request $request)
@@ -40,20 +43,64 @@ class IntendanceController extends Controller
 
 	public function newMenuAction(Request $request)
 	{
+		$menu = new Menu();
+
+		$form = $this->createForm(MenuFormType::class, $menu);
+		
+		if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($menu);
+			$em->flush();
+			
+			$request->getSession()->getFlashBag()->add('success', 'Menu enregistré');
+			
+			return $this->redirectToRoute(
+				'responsabilites_intendance_view_menu',
+				array(
+					'slug' => $menu->getSlug(),
+					)
+				);
+		}
+
 		return $this->render(
 			'ResponsabilitesBundle:Intendance:new_menu.html.twig',
 			array(
-				#
+				'form' => $form->createView(),
 				)
 			);
 	}
 
 	public function editMenuAction(Request $request, $slug)
 	{
+		$em = $this->getDoctrine()->getManager();
+		$menu = $em
+			->getRepository('ResponsabilitesBundle:Menu')
+			->findOneBySlug((string) $slug);
+		
+		if ($menu === null)
+			throw $this->createNotFoundException('Le menu '.$slug.' n\'existe pas');
+		
+		$form = $this->createForm(MenuFormType::class, $menu);
+		
+		if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+			$em->persist($menu);
+			$em->flush();
+			
+			$request->getSession()->getFlashBag()->add('success', 'Menu enregistré');
+			
+			return $this->redirectToRoute(
+				'responsabilites_intendance_view_menu',
+				array(
+					'slug' => $menu->getSlug(),
+					)
+				);
+		}
+		
 		return $this->render(
 			'ResponsabilitesBundle:Intendance:edit_menu.html.twig',
 			array(
-				#
+				'menu' => $menu,
+				'form' => $form->createView(),
 				)
 			);
 	}
