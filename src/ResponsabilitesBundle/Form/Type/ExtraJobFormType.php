dcf85a47735327391092d5a51656c6fbdb9724a5
Nils Van Zuijlen <nilsdu29@gmail.com>
2016-10-15 23:03:12 +0200

    Created ExtraJobFormType.php
    Générateur du formulaire d'ajout/édition de formulaire

---
+++ src/ResponsabilitesBundle/Form/Type/ExtraJobFormType.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace ResponsabilitesBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\DataTransformer\IntegerToLocalizedStringTransformer;
+
+class ExtraJobType extends AbstractType
+{
+	/**
+	 * @param FormBuilderInterface $builder
+	 * @param array $options
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add(
+				'equipe',
+				EntityType::class,
+				array(
+					'label'        => 'Équipe',
+					'required'     => true,
+					'class'        => 'ResponsabilitesBundle:ExtraJob',
+					'choice_label' => 'nom',
+					)
+				)
+			->add(
+				'date',
+				DateType::class,
+				array(
+					'label' => 'Date de l\'extra job',
+					)
+				)
+			->add(
+				'montant',
+				IntegerType::class,
+				array(
+					'label'         => 'Montant gagné',
+					'scale'         => 2,
+					'rounding_mode' => IntegerToLocalizedStringTransformer::ROUND_HALF_DOWN,
+					'attr'          => array(
+						'min' => 0.01,
+						'max' => 99999999.99,
+						'step' => 0.01,
+						),
+					)
+				)
+			->add(
+				'commentaires',
+				TextareaType::class,
+				array(
+					'label' => 'Commentaires'
+					)
+				)
+			->add(
+				'submit', 
+				SubmitType::class,
+				array(
+					'label' => 'Enregistrer',
+					'attr'  => array(
+						'class' => 'btn btn-primary',
+						),
+					)
+				)
+		;
+	}
+	
+	/**
+	 * @param OptionsResolver $resolver
+	 */
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults(array(
+			'data_class' => 'ResponsabilitesBundle\Entity\ExtraJob'
+		));
+	}
+}
