b799488b8c64c96d9d0698b736f45043db567a60
Nils <nilsdu29@gmail.com>
2016-10-15 17:10:08 +0200

    Added viewByYearAction to BudgetController

--- src/ResponsabilitesBundle/Controller/BudgetController.php
+++ src/ResponsabilitesBundle/Controller/BudgetController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ResponsabilitesController\Controller;
+namespace ResponsabilitesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,5 +12,32 @@ class BudgetController extends Controller
 	public function indexAction(Request $request)
 	{
 		return $this->render('ResponsabilitesBundle:Budget:index.html.twig');
+	}
+
+	public function viewExtraJobAction(Request $request, $year = null)
+	{
+		if ($year === null) {
+			if (getdate()['mon'] >= 9) {
+				$year = getdate()['year']-1;
+			} else {
+				$year = getdate()['year'];
+			}
+		}
+
+		$repository = $this
+			->getDoctrine()
+			->getManager()
+			->getRepository('ResponsabilitesBundle:ExtraJob');
+
+		$extraJobs = $repository->getBySchoolYear($year);
+		$recettes  = $repository->getRecettesOfSchoolYear($year);
+
+		return $this->render(
+			'ResponsabilitesBundle:Budget:view_extra_job.html.twig',
+			array(
+				'extraJobs' => $extraJobs,
+				'recettes'  => $recettes,
+				)
+			);
 	}
 }
