39ed8be4a416d6255bd89a8d18489e3eb421e00d
Nils <nilsdu29@gmail.com>
2016-10-15 16:14:30 +0200

    Edited ResponsabilitesBundle\Entity\ExtraJob
    Ajout de propriétés et de contraintes de validation

--- src/ResponsabilitesBundle/Entity/ExtraJob.php
+++ src/ResponsabilitesBundle/Entity/ExtraJob.php
@@ -3,6 +3,8 @@
 namespace ResponsabilitesBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * ExtraJob
@@ -25,15 +27,38 @@ class ExtraJob
 	 * @var string
 	 *
 	 * @ORM\Column(name="montant", type="decimal", precision=10, scale=2)
+	 * @Assert\Range(min=0.01, max=99999999.99, minMessage="Vous êtes sérieux?", maxMessage="Tout ça?! Faites le en deux fois SVP.")
+	 * 
 	 */
 	private $montant;
 
 	/**
 	 * @var \DateTime
 	 *
-	 * @ORM\Column(name="date", type="date")
+	 * @ORM\Column(type="date")
+	 * @Assert\Date()
 	 */
 	private $date;
+
+	/**
+	 * @ORM\ManyToOne(targetEntity="UserBundle\Entity\Group")
+	 * @ORM\JoinColumn(nullable=false)
+	 */
+	private $equipe;
+
+	/**
+	 * @ORM\Column(type="text", nullable=true)
+	 * @Assert\Type("string")
+	 */
+	private $commentaires = '';
+
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(name="slug", type="string", length=255, unique=true)
+	 * @Gedmo\Slug(fields={"titre"}, updatable=false)
+	 */
+	private $slug;
 
 
 	/**
@@ -93,5 +118,76 @@ class ExtraJob
 	{
 		return $this->date;
 	}
-}
 
+	/**
+	 * Set commentaires
+	 *
+	 * @param string $commentaires
+	 *
+	 * @return ExtraJob
+	 */
+	public function setCommentaires($commentaires)
+	{
+		$this->commentaires = $commentaires;
+
+		return $this;
+	}
+
+	/**
+	 * Get commentaires
+	 *
+	 * @return string
+	 */
+	public function getCommentaires()
+	{
+		return $this->commentaires;
+	}
+
+	/**
+	 * Set slug
+	 *
+	 * @param string $slug
+	 *
+	 * @return ExtraJob
+	 */
+	public function setSlug($slug)
+	{
+		$this->slug = $slug;
+
+		return $this;
+	}
+
+	/**
+	 * Get slug
+	 *
+	 * @return string
+	 */
+	public function getSlug()
+	{
+		return $this->slug;
+	}
+
+	/**
+	 * Set equipe
+	 *
+	 * @param \UserBundle\Entity\Group $equipe
+	 *
+	 * @return ExtraJob
+	 */
+	public function setEquipe(\UserBundle\Entity\Group $equipe)
+	{
+		$this->equipe = $equipe;
+
+		return $this;
+	}
+
+	/**
+	 * Get equipe
+	 *
+	 * @return \UserBundle\Entity\Group
+	 */
+	public function getEquipe()
+	{
+		return $this->equipe;
+	}
+}
