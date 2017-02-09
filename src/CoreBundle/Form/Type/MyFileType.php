ecbad27d89bac7c4e0ee58b8981b956ef3b3c98c
Nils Van Zuijlen <nilsdu29@gmail.com>
2017-02-09 12:43:47 +0100

    Update MyFileType.php

--- src/CoreBundle/Form/Type/MyFileType.php
+++ src/CoreBundle/Form/Type/MyFileType.php
@@ -2,7 +2,6 @@
 namespace CoreBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
